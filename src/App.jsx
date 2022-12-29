d24317f1515bd2859cc4dbbfec23c1465f27b9ab
Fernando <nandokferrari@gmail.com>
2022-12-29 20:39:24 -0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>fernandev</h1>
+    </>
+  );
+}
+
+export default App;
